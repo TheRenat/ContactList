efa5d14cc7166cbe550dafc8949038ab224d76c6
TheRenat <therenat@mail.ru>
2022-07-19 20:28:45 +0300

    add directories

---
+++ ContactList/Models/Person.swift
@@ -0,0 +1,13 @@
+//
+//  Person.swift
+//  ContactList
+//
+//  Created by Renat Ibragimov on 19.07.2022.
+//
+
+struct Person {
+    let name: String
+    let surname: String
+    let email: String
+    let phoneNumber: String
+}
---
+++ ContactList/StoreData/PersonData .swift
@@ -0,0 +1,10 @@
+//
+//  PersonData .swift
+//  ContactList
+//
+//  Created by Renat Ibragimov on 19.07.2022.
+//
+
+class PersonData {
+    
+}
