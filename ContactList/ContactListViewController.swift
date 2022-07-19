5fe2d5ce306b52465c16cd1dc0dbb2f5dd4c3a1f
TheRenat <therenat@mail.ru>
2022-07-19 20:13:19 +0300

    add logic

--- ContactList/ContactListViewController.swift
+++ ContactList/ContactListViewController.swift
@@ -33,6 +33,7 @@ class ContactListViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "contact", for: indexPath)
+        cell.textLabel?.text = "Cell index: \(indexPath.row)"
         return cell
     }
 
@@ -71,14 +72,9 @@ class ContactListViewController: UITableViewController {
     }
     */
 
-    /*
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        
     }
-    */
-
 }
