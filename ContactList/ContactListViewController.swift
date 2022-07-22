ba9f8a2f1ac1fa8e5e8fc86973570278d51da164
TheRenat <therenat@mail.ru>
2022-07-22 07:51:43 +0300

    remove some elements

--- ContactList/ContactListViewController.swift
+++ ContactList/ContactListViewController.swift
@@ -14,19 +14,6 @@ class ContactListViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-    }
-
-    // MARK: - Table view data source
-
-//    override func numberOfSections(in tableView: UITableView) -> Int {
-//        return 1
-//    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         person.name.count
     }
@@ -39,10 +26,10 @@ class ContactListViewController: UITableViewController {
 
     // MARK: - Navigation
 
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        guard let fullInfoVC = segue.destination as? FullInfoViewController else { return }
-        guard let indexPath = tableView.indexPathForSelectedRow else { return }
-        fullInfoVC.person = person[indexPath.row]
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        guard let fullInfoVC = segue.destination as? FullInfoViewController else { return }
+//        guard let indexPath = tableView.indexPathForSelectedRow else { return }
+//        fullInfoVC.person = person[indexPath.row]
         
     }
 }
