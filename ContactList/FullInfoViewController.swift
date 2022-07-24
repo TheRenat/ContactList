bd621c0298a09e8350ba4415d90afc5c783ab596
TheRenat <therenat@mail.ru>
2022-07-24 18:54:52 +0300

    restart the project

--- ContactList/FullInfoViewController.swift
+++ ContactList/FullInfoViewController.swift
@@ -9,15 +9,10 @@ import UIKit
 
 class FullInfoViewController: UIViewController {
 
-    @IBOutlet var phoneNumberLabel: UILabel!
-    @IBOutlet var emailLabel: UILabel!
-    
-    var person: Person!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //navigationItem.title = person
-        phoneNumberLabel.text = ""
+
         
 
     }
