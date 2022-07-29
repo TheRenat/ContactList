1b0169518cfd133b9c0fe9dcd7b6b4017ce658bf
TheRenat <therenat@mail.ru>
2022-07-29 21:57:14 +0300

    add logic

--- ContactList/ViewControllers/TabBarViewController.swift
+++ ContactList/ViewControllers/TabBarViewController.swift
@@ -17,6 +17,7 @@ class TabBarViewController: UITabBarController{
     
     private func setupViewControllers() {
         guard let contactListVC = viewControllers?.first as? ContactListViewController else { return }
+        guard let infoVC = viewControllers else { return }
         guard let contactTableVC = viewControllers?.last as? ContactTableViewController else { return }
         
         let persons = Person.getContactPerson()
